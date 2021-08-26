3a0940151fdc784c87daf88decd103015039f043
EliteMasterEric <ericmyllyoja@gmail.com>
2021-08-25 22:23:01 -0400

    Created test song and hid default weeks.

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -28,12 +28,12 @@ class StoryMenuState extends MusicBeatState
 	{
 		return [
 			['Tutorial'],
-			['Bopeebo', 'Fresh', 'Dad Battle'],
-			['Spookeez', 'South', "Monster"],
-			['Pico', 'Philly Nice', "Blammed"],
-			['Satin Panties', "High", "Milf"],
-			['Cocoa', 'Eggnog', 'Winter Horrorland'],
-			['Senpai', 'Roses', 'Thorns']
+			// ['Bopeebo', 'Fresh', 'Dad Battle'],
+			// ['Spookeez', 'South', "Monster"],
+			// ['Pico', 'Philly Nice', "Blammed"],
+			// ['Satin Panties', "High", "Milf"],
+			// ['Cocoa', 'Eggnog', 'Winter Horrorland'],
+			// ['Senpai', 'Roses', 'Thorns']
 		];
 	}
 	var curDifficulty:Int = 1;
@@ -42,12 +42,12 @@ class StoryMenuState extends MusicBeatState
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
-		['dad', 'bf', 'gf'],
-		['spooky', 'bf', 'gf'],
-		['pico', 'bf', 'gf'],
-		['mom', 'bf', 'gf'],
-		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf']
+		// ['dad', 'bf', 'gf'],
+		// ['spooky', 'bf', 'gf'],
+		// ['pico', 'bf', 'gf'],
+		// ['mom', 'bf', 'gf'],
+		// ['parents-christmas', 'bf', 'gf'],
+		// ['senpai', 'bf', 'gf']
 	];
 
 	var weekNames:Array<String> = CoolUtil.coolTextFile(Paths.txt('data/weekNames'));
